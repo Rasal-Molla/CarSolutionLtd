3f3e56ad526ec7c90cf4f08ce83627cc89be4a3a
Rasal-Molla <rasalmolla159401@gmail.com>
2022-10-26 16:21:49 +0600

    Dashboard Change

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -7,10 +7,10 @@
                 <div class="row g-4">
                     <div class="col-sm-6 col-xl-3">
                         <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                            <i class="fa fa-chart-line fa-3x text-primary"></i>
+                            <i class="fa fa-user fa-3x text-primary"></i>
                             <div class="ms-3">
-                                <p class="mb-2">Today Sale</p>
-                                <h6 class="mb-0">$1234</h6>
+                                <p class="mb-2">Total User</p>
+                                <h6 class="mb-0">1234</h6>
                             </div>
                         </div>
                     </div>
@@ -18,8 +18,8 @@
                         <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
                             <i class="fa fa-chart-bar fa-3x text-primary"></i>
                             <div class="ms-3">
-                                <p class="mb-2">Total Sale</p>
-                                <h6 class="mb-0">$1234</h6>
+                                <p class="mb-2">Total Service Center</p>
+                                <h6 class="mb-0">34</h6>
                             </div>
                         </div>
                     </div>
